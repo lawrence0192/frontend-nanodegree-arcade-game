55099ba2d81eb3d692992f36e4b23fd32f23184d
lawrence0192 <lawrence0192@yahoo.com>
2015-02-28 13:06:42 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,119 +1,103 @@
+// Draw the enemy and player objects on the screen
 Object.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-// Enemies our player must avoid
-var Enemy = function() {
-    this.rangeOfx= [-150, 600];
-    this.valuesOfy= [60, 140, 220];
-    this.speed= [150, 600];
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+//Reset player to beginning position
+Object.prototype.reset = function() {
+  player.x = 200;
+  player.y = 400;
+}
+
+/*
+    Enemy Objects
+*/
+
+// Enemies the player must avoid
+var Enemy = function(x,y) {
+
+    // The image/sprite for enemies
     this.sprite = 'images/enemy-bug.png';
-    
-    this.reset();
+
+    //x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 200) + 100);
 }
 
-Enemy.prototype.reset = function(){
-    var startSpot = this.rangeOfx[0];
-
-    this.x = startSpot;
-    this.y = this.getRandomY();
-    this.speed = this.getRandomSpeed();
-}
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    var maxPos = this.rangeOfx[1];
-    this.x += this.speed * dt;
-
-    if (this.x > maxPos) {
-        this.reset();
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x <= 550){
+        this.x += this.speed * dt;
+    }else{
+        this.x = -2;
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            this.reset();
+        }
+    }
 }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+/*
+    Player Object
+*/
 
-Enemy.prototype.getRandomY = function(){
-    return this.valuesOfy[Math.floor(Math.random() * this.valuesOfy.length)];
-}
-
-Enemy.prototype.getRandomSpeed = function() {
-    var minSpeed = this.speed[0],
-    var maxSpeed = this.speed[1];
-
-    return Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
-}
-
-var Player = function() {
-    this.rangeOfx = [-2, 402];
-    this.rangeOfy = [-20, 380];
+// Player class and initial x and y coordinates
+var Player = function(){
     this.sprite = 'images/char-boy.png';
-    this.reset();
-}
-
-Player.prototype.update = function() {
-    this.checkCollisions();
-}
-
-Player.prototype.checkCollisions = function() {
-    if (this.y == -20) {
-        // player is on water, reset
-        this.reset();
-    } else if (this.y >= 60 && this.y <= 220) {
-        var self = this;
-        // player is on road rows, check collisions       
-        // loop through each bug
-        allEnemies.forEach(function(enemy) {
-            // is the bug on the same row as the player?
-            if (enemy.y == self.y) {
-                // is the bug on the player?
-                if (enemy.x >= player.x - 30 && enemy.x <= player.x + 30) {
-                    self.reset();
-                }
-            }
-        });
-    }
-}
-
-Player.prototype.reset = function() {
     this.x = 200;
-    this.y = 380;
+    this.y = 400;
 }
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-Player.prototype.handleInput = function(key) {
-    if (key === 'left') {
-        this.x -= (this.x - 101 < this.rangeOfx[0]) ? 0 : 101;
-    } else if (key === 'right') {
-        this.x += (this.x + 101 > this.rangeOfx[1]) ? 0 : 101;
-    } else if (key === 'up') {
-        this.y -= (this.y - 80 < this.rangeOfy[0]) ? 0 : 80;
-    } else if (key === 'down') {
-        this.y += (this.y + 80 > this.rangeOfy[1]) ? 0 : 80;
+//Update player position
+Player.prototype.update = function(){
+    //if left key is pressed and player is not on edge of map, pressed decrement x
+    if(this.ctlKey === 'left' && this.x > 0){ 
+        this.x = this.x - 50;
+    //if right key is pressed and player is not on edge of map increment x 
+    }else if(this.ctlKey === 'right' && this.x != 400){
+        this.x = this.x + 50;
+    //if up key is pressed increment y 
+    }else if(this.ctlKey === 'up'){
+        this.y = this.y - 50;
+    //if down key is pressed and player is not on edge of map decrement y 
+    }else if (this.ctlKey === 'down' && this.y != 400){
+        this.y = this.y + 50;
+    }
+    this.ctlKey = null;
+    
+    //If on water, reset
+    if(this.y < 25){
+        this.reset();
     }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
+//Input handler for player
+Player.prototype.handleInput = function(e){
+    this.ctlKey = e;    
+}
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Instantiate enemies and player objects
+var allEnemies = [];
+(function setEnemies(){
+    allEnemies.push(new Enemy(-2, 60));
+    allEnemies.push(new Enemy(-2, 100));
+    allEnemies.push(new Enemy(-2,150));
+    allEnemies.push(new Enemy(-2,220));
+}());
+
+var player = new Player(); 
+
+
+// listens for key presses and sends the keys to 
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -124,10 +108,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-var Lawrence = new Enemy();
-var Britt = new Enemy();
-var Dale = new Enemy();
-var allEnemies = [Lawrence, Britt, Dale];
-
-var player = new Player();
