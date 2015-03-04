11da8ee2e565feb3fd77a4639fbff331e445cefa
lawrence0192 <lawrence0192@yahoo.com>
2015-03-04 12:38:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -77,10 +77,10 @@ Player.prototype.handleInput = function(e){
 // Instantiate enemies and player objects
 var allEnemies = [];
 (function setEnemies(){
-    allEnemies.push(new Enemy(0, 60));
-    allEnemies.push(new Enemy(0, 100));
-    allEnemies.push(new Enemy(0,150));
-    allEnemies.push(new Enemy(0,220));
+    allEnemies.push(new Enemy(-10, 60));
+    allEnemies.push(new Enemy(-10, 100));
+    allEnemies.push(new Enemy(-10,150));
+    allEnemies.push(new Enemy(-10,220));
 }());
 
 var player = new Player(); 
